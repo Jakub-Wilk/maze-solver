1abe7b655213cc7e2f39f490ce8b4322b37a02ff
Jakub Wilk <wilkjakub64@gmail.com>
2021-09-12 22:59:09 +0200

    Added argument parsing using argparse

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+from __future__ import annotations
+
+import argparse
 import sys
 
+from PIL import Image
 
-def main(*args):
-    pass
+from modules.Maze import Maze
+
+
+def main(path: str) -> None:
+    image = Image.open(path)
+    maze = Maze.create_from_image(image)
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    parser = argparse.ArgumentParser(description="A rudimentary maze solver")
+    parser.add_argument('-p', '--path', default="mazes/test_maze.png", help="The path to the image containing the maze")
+    args = parser.parse_args()
+    main(args.path)
