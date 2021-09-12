7a8300a56ab212cd3b7c9f703bb6a8f620624820
Jakub Wilk <wilkjakub64@gmail.com>
2021-09-13 01:06:20 +0200

    Added checks for begin and end arguments; Will throw IndexError if out of bounds

--- modules/Solver.py
+++ modules/Solver.py
@@ -1,9 +1,28 @@
 class Solver:
 
     def __init__(self, maze, begin, end, animate, steps, output):
+
         self.maze = maze
+        # set maze_max_coords to the bottom right pixel coordinates
+        maze_max_coords = (len(maze.grid) - 1, len(maze.grid[-1]) - 1)
+
         self.begin = begin
-        self.end = (len(maze) - 1, len(maze[-1]) - 1) if end is None else end
+
+        # check if begin is in bounds
+        if self.begin[0] < 0 or self.begin[0] > maze_max_coords[0]:
+            raise IndexError("The beginning x coordinate is outside the image!")
+        if self.begin[1] < 0 or self.begin[1] > maze_max_coords[1]:
+            raise IndexError("The beginning y coordinate is outside the image!")
+
+        # if end is not set, set it to bottom right
+        self.end = maze_max_coords if end is None else end
+
+        # check if end is in bounds
+        if self.end[0] < 0 or self.end[0] > maze_max_coords[0]:
+            raise IndexError("The ending x coordinate is outside the image!")
+        if self.end[1] < 0 or self.end[1] > maze_max_coords[1]:
+            raise IndexError("The ending y coordinate is outside the image!")
+
         self.animate = animate
         self.steps = steps
         self.output = output
