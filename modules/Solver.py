a7300a1017cd8da4397c3e28cd3438c23766b5c3
Jakub Wilk <wilkjakub64@gmail.com>
2021-09-13 02:00:18 +0200

    Added Solver.is_crossroads()

--- modules/Solver.py
+++ modules/Solver.py
@@ -26,5 +26,17 @@ class Solver:
         self.steps = steps
         self.output = output
 
+    def is_crossroads(self, position: tuple[int, int]) -> bool:
+        # this function returns True if the current tile has three or four neighbouring traversable tiles
+        x, y = position
+        north = 1 if self.maze.get_tile_at((x, y + 1)) else 0
+        east = 1 if self.maze.get_tile_at((x + 1, y)) else 0
+        south = 1 if self.maze.get_tile_at((x, y - 1)) else 0
+        west = 1 if self.maze.get_tile_at((x - 1, y)) else 0
+        if north + east + south + west > 2:
+            return True
+        else:
+            return False
+
     def solve(self) -> None:
         pass
